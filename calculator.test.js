5d0ab90337502a57653632724e05366094e58ee7
Noah Mekonnen <noahmekonnen0@gmail.com>
2024-02-05 09:49:57 -0500

    attempt to test route

--- calculator.test.js
+++ calculator.test.js
@@ -2,8 +2,7 @@ const app = require('calculator')
 describe("Testing the routes", function(){
     test("Checking the /mean route",function(){
         request(app)
-        .get('/mean')
-        .send({nums:[1,2,3]})
+        .get('/mean?nums=1,2,3')
         .expect({
             operation: 'mean',
             value: 2
